2fab0e31e6d6bcc1fb53998f06e1a42703f00190
samet <sametaydin1997@gmail.com>
2019-06-13 15:09:56 +0300

    last commit reverted

--- screenshot.js
+++ screenshot.js
@@ -2,36 +2,32 @@ const puppeteer = require('puppeteer');
 const log4js = require('log4js');
 
 log4js.configure({
-    appenders: {ss: { type: 'file', filename: `screenshotjs.log` } },
-    categories: { default: { appenders: ['ss'], level: 'debug' } }
+    appenders: {ss: {type: 'file', filename: `screenshotjs.log`}},
+    categories: {default: {appenders: ['ss'], level: 'debug'}}
 });
 
 const logger = log4js.getLogger('ss');
 
 (async () => {
+    const browser = await puppeteer.launch({defaultViewport: null});
+    const page = await browser.newPage();
+
+    let argv = require('minimist')(process.argv.slice(2));
+    let queryUrl = argv['url'];
+    let fileName = argv['filename'];
+
+    logger.info("URL: ", queryUrl);
+    logger.debug("File Name: ", fileName);
+
+    await page.setViewport({width: 1920, height: 1080});
     try {
-        const browser = await puppeteer.launch({defaultViewport: null});
-        const page = await browser.newPage();
-
-        let argv = require('minimist')(process.argv.slice(2));
-        let queryUrl = argv['url'];
-        let fileName = argv['filename'];
-
-        logger.info("URL: ", queryUrl);
-        logger.debug("File Name: ", fileName);
-
-        await page.setViewport({width: 1920, height: 1080});
-        try {
-            await page.goto(queryUrl, {waitUntil: 'networkidle0'});
-            await page.screenshot({path: fileName, fullPage: true});
-            logger.info("Screenshot has saved!")
-        } catch (ERR_CERT_COMMON_NAME_INVALID) {
-            logger.warn('This URL is not valid!');
-        } finally {
-            await browser.close();
-            logger.debug("Browser has closed!")
-        }
-    } catch (e) {
-        logger.warn("Error caught!");
+        await page.goto(queryUrl, {waitUntil: 'networkidle0'});
+        await page.screenshot({path: fileName, fullPage: true});
+        logger.info("Screenshot has saved!")
+    } catch (ERR_CERT_COMMON_NAME_INVALID) {
+        logger.warn('This URL is not valid!');
+    } finally {
+        await browser.close();
+        logger.debug("Browser has closed!")
     }
 })();
