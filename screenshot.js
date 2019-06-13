296066aae669d2f93062f2829f17b8f3e5934d91
samet <sametaydin1997@gmail.com>
2019-06-13 15:06:59 +0300

    async func try-catch

--- screenshot.js
+++ screenshot.js
@@ -9,25 +9,29 @@ log4js.configure({
 const logger = log4js.getLogger('ss');
 
 (async () => {
-    const browser = await puppeteer.launch({defaultViewport: null});
-    const page = await browser.newPage();
-
-    let argv = require('minimist')(process.argv.slice(2));
-    let queryUrl = argv['url'];
-    let fileName = argv['filename'];
-
-    logger.info("URL: ", queryUrl);
-    logger.debug("File Name: ", fileName);
-
-    await page.setViewport({width: 1920, height: 1080});
     try {
-        await page.goto(queryUrl, {waitUntil: 'networkidle0'});
-        await page.screenshot({path: fileName, fullPage: true});
-        logger.info("Screenshot has saved!")
-    } catch (ERR_CERT_COMMON_NAME_INVALID) {
-        logger.warn('This URL is not valid!');
-    } finally {
-        await browser.close();
-        logger.debug("Browser has closed!")
+        const browser = await puppeteer.launch({defaultViewport: null});
+        const page = await browser.newPage();
+
+        let argv = require('minimist')(process.argv.slice(2));
+        let queryUrl = argv['url'];
+        let fileName = argv['filename'];
+
+        logger.info("URL: ", queryUrl);
+        logger.debug("File Name: ", fileName);
+
+        await page.setViewport({width: 1920, height: 1080});
+        try {
+            await page.goto(queryUrl, {waitUntil: 'networkidle0'});
+            await page.screenshot({path: fileName, fullPage: true});
+            logger.info("Screenshot has saved!")
+        } catch (ERR_CERT_COMMON_NAME_INVALID) {
+            logger.warn('This URL is not valid!');
+        } finally {
+            await browser.close();
+            logger.debug("Browser has closed!")
+        }
+    } catch (e) {
+        logger.warn("Error caught!");
     }
 })();
